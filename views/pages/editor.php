6718254d4aa449fa42335bd6cb8bf56bf6f330df
Jimmy Charnley Kromann <jimmy@charnley.dk>
2014-03-30 18:19:25 +0200

    htaccess gitignore editor

--- views/pages/editor.php
+++ views/pages/editor.php
@@ -113,11 +113,10 @@ var myInfo1 = {
 
                         <div class="editorset">
                             <div class="actions center calculation">
-                                <ul style="display:none;">
+                                <ul style="">
                                     <li class="action name"><a rel="" class="green button"><span class="hvrHlpCnt "><span class="hvrHlp">Get the name of current structure</span></span><span class="text">Find Name</span></a></li>
+                                    <li><a class="green button calculate"><span class="hvrHlpCnt"><span class="hvrHlp">Is it time to calculate some properties?</span></span><span class="text">Calculate Properties</span></a></li>
                                 </ul>
-                                <br />
-                                <a class="green button calculate"><span class="hvrHlpCnt"><span class="hvrHlp">Is it time to calculate some properties?</span></span><span class="text">Calculate Properties</span></a>
                             </div>
                         </div>
 
