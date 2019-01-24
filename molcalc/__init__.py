ff781498117066cd1ec1abdde48f33d9a1f140e0
Jimmy Charnley Kromann <jimmy@charnley.dk>
2019-01-24 13:40:00 +0100

    database init

--- molcalc/__init__.py
+++ molcalc/__init__.py
@@ -11,6 +11,52 @@ from wsgiref.simple_server import make_server
 
 from pyramid_jinja2.filters import static_url_filter
 
+
+from sqlalchemy import engine_from_config
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import configure_mappers
+import zope.sqlalchemy
+
+import models
+
+
+def get_engine(settings, prefix='sqlalchemy.'):
+    return engine_from_config(settings, prefix)
+
+
+def get_session_factory(engine):
+    factory = sessionmaker()
+    factory.configure(bind=engine)
+    return factory
+
+
+def get_tm_session(session_factory, transaction_manager):
+    """
+    Get a ``sqlalchemy.orm.Session`` instance backed by a transaction.
+
+    This function will hook the session to the transaction manager which
+    will take care of committing any changes.
+
+    - When using pyramid_tm it will automatically be committed or aborted
+      depending on whether an exception is raised.
+
+    - When using scripts you should wrap the session in a manager yourself.
+      For example::
+
+          import transaction
+
+          engine = get_engine(settings)
+          session_factory = get_session_factory(engine)
+          with transaction.manager:
+              dbsession = get_tm_session(session_factory, transaction.manager)
+
+    """
+    dbsession = session_factory()
+    zope.sqlalchemy.register(
+        dbsession, transaction_manager=transaction_manager)
+    return dbsession
+
+
 def get_config(config=None):
     """ This function defines the configuration of the site
     """
@@ -42,6 +88,33 @@ def get_config(config=None):
     config.add_route('smiles_to_sdf', '/ajax/smiles')
     config.add_route('sdf_to_smiles', '/ajax/sdf')
 
+
+    # database
+    settings = config.get_settings()
+    settings['tm.manager_hook'] = 'pyramid_tm.explicit_manager'
+
+    # use pyramid_tm to hook the transaction lifecycle to the request
+    config.include('pyramid_tm')
+
+    # use pyramid_retry to retry a request when transient exceptions occur
+    config.include('pyramid_retry')
+
+    #
+    engine = get_engine(settings)
+    models.initialize_db(engine)
+
+    session_factory = get_session_factory(engine)
+    config.registry['dbsession_factory'] = session_factory
+
+    # make request.dbsession available for use in Pyramid
+    config.add_request_method(
+        # r.tm is the transaction manager used by pyramid_tm
+        lambda r: get_tm_session(session_factory, r.tm),
+        'dbsession',
+        reify=True
+    )
+
+
     # Scan a Python package and any of its subpackages for objects marked with
     # configuration decoration
     config.scan()
@@ -56,11 +129,13 @@ def get_config(config=None):
 
     return config
 
+from sqlalchemy import engine_from_config
 
 def main(global_settings, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = get_config(config=Configurator(settings=settings))
+
     return config.make_wsgi_app()
 
 
