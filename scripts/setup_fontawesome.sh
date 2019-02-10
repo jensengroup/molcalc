cccf81c5cea106928d74fe12e34ab115c4d1781a
Jimmy Charnley Kromann <jimmy@charnley.dk>
2019-02-10 19:03:42 +0100

    setups

---
+++ scripts/setup_fontawesome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://use.fontawesome.com/releases/v5.7.1/fontawesome-free-5.7.1-web.zip
+
+mv fontawesome-free-5.7.1-web fontawesome
+
+
---
+++ scripts/setup_jquery.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://code.jquery.com/jquery-3.3.1.min.js
+
+mv jquery-3.3.1.min.js js
+
+
+
